4ecc7707eb8b178b5ebec4e73517a8b2a28b82ef
dapema01 <davidmalmberg01@gmail.com>
2024-12-09 15:24:04 +0100

    David och Hannas changes så att man bara kan svara en gång

--- src/views/PollView.vue
+++ src/views/PollView.vue
@@ -1,75 +1,82 @@
 <template>
-<Nav :hideNav="false" 
-      :uiLabels="uiLabels" 
-      :lang="lang"
-      @language-changed="handleLanguageChange">
-    </Nav>
+  <Nav :hideNav="false"
+  :uiLabels="uiLabels"
+  :lang="lang"
+  @language-changed="handleLanguageChange">
+  </Nav>
   <div>
-
-    <h1>{{uiLabels.Welcome}}</h1>
-    {{pollId}}
-    <QuestionComponent v-bind:question="question"
-              v-on:answer="submitAnswer($event)"/>
-    <hr>
-    <span>{{submittedAnswers}}</span>
-
-
+  
+  <h1>{{uiLabels.Welcome}}</h1>
+  {{pollId}}
+  <QuestionComponent v-bind:question="question"
+  v-on:answer="submitAnswer($event)"/>
+  <hr>
+  <span>{{submittedAnswers}}</span>
+  
+  
   </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import Nav from '@/components/ResponsiveNav.vue'
-import QuestionComponent from '@/components/QuestionComponent.vue';
-import io from 'socket.io-client';
-
-const socket = io("localhost:3000");
-
-export default {
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import Nav from '@/components/ResponsiveNav.vue'
+  import QuestionComponent from '@/components/QuestionComponent.vue';
+  import io from 'socket.io-client';
+  
+  const socket = io("localhost:3000");
+  
+  export default {
   name: 'PollView',
   components: {
-    QuestionComponent, 
-    Nav,
+  QuestionComponent,
+  Nav,
   },
   data: function () {
-    return {
-      Welcome: "Welcome to the poll!",
-      question: {
-        q: "",
-        a: []
-      },
-      pollId: "inactive poll",
-      submittedAnswers: {},
-      uiLabels: {},
-      lang: localStorage.getItem("lang") || "en",
-    }
+  return {
+  Welcome: "Welcome to the poll!",
+  question: {
+  q: "",
+  a: []
+  },
+  pollId: "inactive poll",
+  submittedAnswers: {},
+  uiLabels: {},
+  lang: localStorage.getItem("lang") || "en",
+  hasAnswerd: false,
+  }
   },
   created: function () {
-    this.pollId = this.$route.params.id;
-    socket.on( "questionUpdate", q => this.question = q );
-    socket.on( "submittedAnswersUpdate", answers => this.submittedAnswers = answers );
-    socket.on( "uiLabels", labels => this.uiLabels = labels );
-    socket.emit( "getUILabels", this.lang );
-    socket.emit( "joinPoll", this.pollId );
+  this.pollId = this.$route.params.id;
+  socket.on( "questionUpdate", q => this.question = q );
+  socket.on( "submittedAnswersUpdate", answers => this.submittedAnswers = answers );
+  socket.on( "uiLabels", labels => this.uiLabels = labels );
+  socket.emit( "getUILabels", this.lang );
+  socket.emit( "joinPoll", this.pollId );
   },
   methods: {
-    submitAnswer: function (answer) {
-      socket.emit("submitAnswer", {pollId: this.pollId, answer: answer})
-    },
-    handleLanguageChange(newLang) {
-      this.lang = newLang;
-      localStorage.setItem("lang", newLang);
-      socket.emit("getUILabels", this.lang);
-    }
+  submitAnswer: function (answer) {
+  
+  if (this.hasAnswerd) {
+  alert("You have already answered this question")
+  return;
   }
-}
-</script>
-
-
-
-<style scoped>
-h1 {
+  this.hasAnswerd = true;
+  socket.emit("submitAnswer", {pollId: this.pollId, answer: answer})
+  },
+  handleLanguageChange(newLang) {
+  this.lang = newLang;
+  localStorage.setItem("lang", newLang);
+  socket.emit("getUILabels", this.lang);
+  }
+  }
+  }
+  </script>
+  
+  
+  
+  <style scoped>
+  h1 {
   margin-top: 0;
   color: black;
-}
-</style>
+  }
+  </style>
