615512347b24c22a26e88996ea5ffa05ec964fe8
sebberendel <sebbe.rendel@gmail.com>
2024-12-11 15:42:15 +0100

    Bug fix: Answeres were not saved

--- server/Data.js
+++ server/Data.js
@@ -33,7 +33,7 @@ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
 ***********************************************/
 
 Data.prototype.pollExists = function (pollId) {
-return typeof this.polls[pollId] !== "undefined"
+  return typeof this.polls[pollId] !== "undefined"
 }
 
 Data.prototype.generateGamePin = function () {
@@ -65,105 +65,97 @@ Data.prototype.storeGameData = function (gameData){
 
 
 Data.prototype.getUILabels = function (lang) {
-//check if lang is valid before trying to load the dictionary file
-if (!["en", "sv"].some( el => el === lang))
-lang = "en";
-const labels = readFileSync("./server/data/labels-" + lang + ".json");
-return JSON.parse(labels);
+  //check if lang is valid before trying to load the dictionary file
+  if (!["en", "sv"].some( el => el === lang))
+    lang = "en";
+  const labels = readFileSync("./server/data/labels-" + lang + ".json");
+  return JSON.parse(labels);
 }
 
-Data.prototype.createPoll = function (pollId, lang = "en") {
-if (this.pollExists(pollId)) {
-// Återställ data för befintlig omröstning
-//this.polls[pollId].questions = [];
-this.polls[pollId].answers = [];
-this.polls[pollId].participants = [];
-this.polls[pollId].currentQuestion = 0;
-console.log("poll reset", pollId);
-} else {
-// Skapa en ny omröstning
-let poll = {};
-poll.lang = lang;
-//poll.questions = [];
-poll.answers = [];
-poll.participants = [];
-poll.currentQuestion = 0;
-this.polls[pollId] = poll;
-console.log("poll created", pollId, poll);
+Data.prototype.createPoll = function(pollId, lang="en") {
+  if (!this.pollExists(pollId)) {
+    let poll = {};
+    poll.lang = lang;  
+    poll.questions = [];
+    poll.answers = [];
+    poll.participants = [];
+    poll.currentQuestion = 0;              
+    this.polls[pollId] = poll;
+    console.log("poll created", pollId, poll);
+  }
+  return this.polls[pollId];
 }
-return this.polls[pollId];
-};
 
 Data.prototype.getPoll = function(pollId) {
-if (this.pollExists(pollId)) {
-return this.polls[pollId];
-}
-return {};
+  if (this.pollExists(pollId)) {
+    return this.polls[pollId];
+  }
+  return {};
 }
 
 Data.prototype.participateInPoll = function(pollId, name) {
-console.log("participant will be added to", pollId, name);
-if (this.pollExists(pollId)) {
-this.polls[pollId].participants.push({name: name, answers: []})
-}
+  console.log("participant will be added to", pollId, name);
+  if (this.pollExists(pollId)) {
+    this.polls[pollId].participants.push({name: name, answers: []})
+  }
 }
 
 Data.prototype.getParticipants = function(pollId) {
-const poll = this.polls[pollId];
-console.log("participants requested for", pollId);
-if (this.pollExists(pollId)) {
-return this.polls[pollId].participants
-}
-return [];
+  const poll = this.polls[pollId];
+  console.log("participants requested for", pollId);
+  if (this.pollExists(pollId)) { 
+    return this.polls[pollId].participants
+  }
+  return [];
 }
 
 Data.prototype.addQuestion = function(pollId, q) {
-if (this.pollExists(pollId)) {
-this.polls[pollId].questions.push(q);
-}
+  if (this.pollExists(pollId)) {
+    this.polls[pollId].questions.push(q);
+  }
 }
 
 Data.prototype.getQuestion = function(pollId, qId = null) {
-if (this.pollExists(pollId)) {
-const poll = this.polls[pollId];
-if (qId !== null) {
-poll.currentQuestion = qId;
-}
-return poll.questions[poll.currentQuestion];
-}
-return {}
+  if (this.pollExists(pollId)) {
+    const poll = this.polls[pollId];
+    if (qId !== null) {
+      poll.currentQuestion = qId;
+    }
+    return poll.questions[poll.currentQuestion];
+  }
+  return {}
 }
 
 Data.prototype.getSubmittedAnswers = function(pollId) {
-if (this.pollExists(pollId)) {
-const poll = this.polls[pollId];
-const answers = poll.answers[poll.currentQuestion];
-if (typeof poll.questions[poll.currentQuestion] !== 'undefined') {
-return answers;
-}
-}
-return {}
+  if (this.pollExists(pollId)) {
+    const poll = this.polls[pollId];
+    const answers = poll.answers[poll.currentQuestion];
+    if (typeof poll.questions[poll.currentQuestion] !== 'undefined') {
+      return answers;
+    }
+  }
+  return {}
 }
 
 Data.prototype.submitAnswer = function(pollId, answer) {
-if (this.pollExists(pollId)) {
-const poll = this.polls[pollId];
-let answers = poll.answers[poll.currentQuestion];
-// create answers object if no answers have yet been submitted
-if (typeof answers !== 'object') {
-answers = {};
-answers[answer] = 1;
-poll.answers.push(answers);
-}
-// create answer property if that specific answer has not yet been submitted
-else if (typeof answers[answer] === 'undefined') {
-answers[answer] = 1;
-}
-// if the property already exists, increase the number
-else
-answers[answer] += 1
-console.log("answers looks like ", answers, typeof answers);
-}
+  if (this.pollExists(pollId)) {
+    const poll = this.polls[pollId];
+    let answers = poll.answers[poll.currentQuestion];
+    // create answers object if no answers have yet been submitted
+    if (typeof answers !== 'object') {
+      answers = {};
+      answers[answer] = 1;
+      poll.answers.push(answers);
+    }
+    // create answer property if that specific answer has not yet been submitted
+    else if (typeof answers[answer] === 'undefined') {
+      answers[answer] = 1;
+    }
+    // if the property already exists, increase the number
+    else
+      answers[answer] += 1
+    console.log("answers looks like ", answers, typeof answers);
+  }
 }
 
 export { Data };
--- src/views/PollView.vue
+++ src/views/PollView.vue
@@ -1,20 +1,20 @@
 <template>
-  <Nav :hideNav="false"
-  :uiLabels="uiLabels"
-  :lang="lang"
-  @language-changed="handleLanguageChange">
-  </Nav>
-  <div>
+  <Nav :hideNav="false" 
+        :uiLabels="uiLabels" 
+        :lang="lang"
+        @language-changed="handleLanguageChange">
+      </Nav>
+    <div>
   
-  <h1>{{uiLabels.Welcome}}</h1>
-  {{pollId}}
-  <QuestionComponent v-bind:question="question"
-  v-on:answer="submitAnswer($event)"/>
-  <hr>
-  <span>{{submittedAnswers}}</span>
+      <h1>{{uiLabels.Welcome}}</h1>
+      {{pollId}}
+      <QuestionComponent v-bind:question="question"
+                v-on:answer="submitAnswer($event)"/>
+      <hr>
+      <span>{{submittedAnswers}}</span>
   
   
-  </div>
+    </div>
   </template>
   
   <script>
@@ -26,51 +26,42 @@
   const socket = io("localhost:3000");
   
   export default {
-  name: 'PollView',
-  components: {
-  QuestionComponent,
-  Nav,
-  },
-  data: function () {
-  return {
-  Welcome: "Welcome to the poll!",
-  question: {
-  q: "",
-  a: []
-  },
-  pollId: "inactive poll",
-  submittedAnswers: {},
-  uiLabels: {},
-  lang: localStorage.getItem("lang") || "en",
-  hasAnswerd: false,
-  }
-  },
-  created: function () {
-    this.pollId = this.$route.params.id;
-    socket.on( "questionUpdate", q => this.question = q );
-    socket.on( "submittedAnswersUpdate", answers => this.submittedAnswers = answers );
-    socket.on( "uiLabels", labels => this.uiLabels = labels );
-    
-    socket.emit( "getUILabels", this.lang );
-    socket.emit( "joinPoll", this.pollId );
-  },
-  methods: {
-  submitAnswer: function (answer) {
-  
-  if (this.hasAnswerd) {
-  alert("You have already answered this question")
-  return;
-  }
-  this.hasAnswerd = true;
-  console.log("Submitting answer for pollId:", this.pollId); // Debug
-  socket.emit("submitAnswer", {pollId: this.pollId, answer: answer})
-  },
-  handleLanguageChange(newLang) {
-  this.lang = newLang;
-  localStorage.setItem("lang", newLang);
-  socket.emit("getUILabels", this.lang);
-  }
-  }
+    name: 'PollView',
+    components: {
+      QuestionComponent, 
+      Nav,
+    },
+    data: function () {
+      return {
+        Welcome: "Welcome to the poll!",
+        question: {
+          q: "",
+          a: []
+        },
+        pollId: "inactive poll",
+        submittedAnswers: {},
+        uiLabels: {},
+        lang: localStorage.getItem("lang") || "en",
+      }
+    },
+    created: function () {
+      this.pollId = this.$route.params.id;
+      socket.on( "questionUpdate", q => this.question = q );
+      socket.on( "submittedAnswersUpdate", answers => this.submittedAnswers = answers );
+      socket.on( "uiLabels", labels => this.uiLabels = labels );
+      socket.emit( "getUILabels", this.lang );
+      socket.emit( "joinPoll", this.pollId );
+    },
+    methods: {
+      submitAnswer: function (answer) {
+        socket.emit("submitAnswer", {pollId: this.pollId, answer: answer})
+      },
+      handleLanguageChange(newLang) {
+        this.lang = newLang;
+        localStorage.setItem("lang", newLang);
+        socket.emit("getUILabels", this.lang);
+      }
+    }
   }
   </script>
   
@@ -78,7 +69,7 @@
   
   <style scoped>
   h1 {
-  margin-top: 10px;
-  color: black;
+    margin-top: 0;
+    color: black;
   }
   </style>
