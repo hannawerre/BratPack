72840a32f70198bb6b772fd17dfd7ef5dc00e523
sebberendel <sebbe.rendel@gmail.com>
2025-01-13 22:06:15 +0100

    Cleanup in LobbyView, ScoreBoard, ResponsiveNav and Data.js

--- server/Data.js
+++ server/Data.js
@@ -1,10 +1,9 @@
 'use strict';
 import {readFileSync} from "fs";
 
-
 // Store data in an object to keep the global namespace clean. In an actual implementation this would be interfacing a database...
 function Data() {
-  // TODO: Uppdatera customgames['test'] så att den ser exakt ut som ett äkta spel
+  
   // Custom Game
   this.countDown = null;
   this.customGames = {};
@@ -33,30 +32,31 @@ Data.prototype.generateGamePin = function () {
  let pin;
  do {
    pin = Math.floor(100000 + Math.random() * 900000).toString();
- } while (this.customGameExists(pin)); // Säkerställ att PIN är unik... behövs detta? Och är den inte bakåtvänd? /sebbe
+ } while (this.customGameExists(pin)); 
  return pin;
 };
-Data.prototype.createCustomGame = function () { // lang = "en" ???
+Data.prototype.createCustomGame = function () { 
+  
   const pin = this.generateGamePin(); 
-  // TODO: If there currently is a game with the same pin. The older game will be overwritten. This issue should probably be solved in CustomGamesView.vue /sebbe
   let customGame = {};
  
   customGame.participants = [];
   customGame.selectedGames = [];
-  customGame.selectedMinutes = 10; // 60 minutes default
-  customGame.remainingTime = 3600; //
+  customGame.selectedMinutes = 10;
+  customGame.remainingTime = 3600; 
   customGame.gameStarted = false;
   customGame.customQuestions = {};
   customGame.useCustomQuestions = false;
 
   this.customGames[pin] = customGame;
   
-  console.log("Custom Game created", pin, customGame);
+  console.log("Custom Game created: ", pin, customGame);
   return { pin, customGame };
 };
 
-Data.prototype.createCustomGameAlt = function (pin, lang) { // lang = "en" ???
-  // TODO: If there currently is a game with the same pin. The older game will be overwritten. This issue should probably be solved in CustomGamesView.vue /sebbe
+// Extreme corner case: If user joins a custom game by typing in a non existing pin in URL
+Data.prototype.createCustomGameAlt = function (pin, lang) {
+  
   let customGame = {};
   customGame.lang = lang;  
   customGame.participants = [];
@@ -64,8 +64,6 @@ Data.prototype.createCustomGameAlt = function (pin, lang) { // lang = "en" ???
   customGame.selectedMinutes = 10; // 60 minutes default
   customGame.remainingTime = 3600; //
   customGame.gameStarted = false;
-  // customGame.customQuestions = {};
-  // customGame.useCustomQuestions = false;
 
   this.customGames[pin] = customGame;
   
@@ -74,15 +72,12 @@ Data.prototype.createCustomGameAlt = function (pin, lang) { // lang = "en" ???
 };
 
 Data.prototype.storeGameDataAndStart = function (gameData){
+  
   // Update the gameData and set gameStarted = true
-  // console.log("gamedata:",gameData);
-  // console.log("this.customgames",this.customGames)
- 
-  // customGame.lang = gameData.lang; // För närvarande skickas denna inte.. för den finns inte i CustomGamesView.vue
   this.customGames[gameData.gamePin].participants = gameData.participants;
   this.customGames[gameData.gamePin].selectedGames = gameData.selectedGames;
   this.customGames[gameData.gamePin].selectedMinutes = gameData.selectedMinutes;
-  this.customGames[gameData.gamePin].remainingTime = gameData.selectedMinutes*60; // Convert to seconds
+  this.customGames[gameData.gamePin].remainingTime = gameData.selectedMinutes*60; 
   this.customGames[gameData.gamePin].gameStarted = true;
   
   // If countdown is not already on -> start the countdown.
@@ -91,32 +86,30 @@ Data.prototype.storeGameDataAndStart = function (gameData){
   console.log("Reached to data.storeGameDataAndStart with current customGames: ", this.customGames[gameData.gamePin]);
 };
 
-
 Data.prototype.getGameData = function(gamePin) {
   if (this.customGameExists(gamePin)) { 
     console.log("gameData: ", this.customGames[gamePin]," requested for custom game: ", gamePin);
     return this.customGames[gamePin];
   }
   console.log("Requested gameData for non existing game... returning []")
-  return []; //maybe returning an array is not the most convenient way... 
+  return [];
 };
 Data.prototype.getCustomGameParticipants = function(gamePin) {
  if (this.customGameExists(gamePin)) {
    console.log("participants requested for custom game: ", gamePin);
    return this.customGames[gamePin].participants;
  }
- return [];  //maybe returning an array is not the most convenient way...
+ return []; 
 };
 
 Data.prototype.participateInCustomGame = function (gamePin, playerObj) {
   if (this.customGameExists(gamePin)) {
     const game = this.customGames[gamePin];
     game.participants.push({
-      name: playerObj.name || "Anonymous", // Anonymous??? ta bort eller?
+      name: playerObj.name,
       scoreGame1: playerObj.scoreGame1 || 0,
       scoreGame2: playerObj.scoreGame2 || 0,
-      scoreGame3: playerObj.scoreGame3 || 0,
-      scoreGame4: playerObj.scoreGame4 || 0
+      scoreGame3: playerObj.scoreGame3 || 0
     });
     console.log("Updated participants:", game.participants);
   } else {
@@ -150,7 +143,7 @@ Data.prototype.deleteUser = function (gamePin, userName) {
 };
 
 Data.prototype.getUILabels = function (lang) {
-  //check if lang is valid before trying to load the dictionary file
+  // Check if lang is valid before trying to load the dictionary file
   if (!["en", "sv"].some( el => el === lang))
     lang = "en";
   const labels = readFileSync("./server/data/labels-" + lang + ".json");
@@ -158,23 +151,22 @@ Data.prototype.getUILabels = function (lang) {
 };
 
 Data.prototype.getQuestions = function (lang, gamePin, whichQuiz) {
-  // Kontrollera om språket är giltigt, annars standard till engelska
+  // Check if lang is valid before trying to load the dictionary file
   if (!["en", "sv"].includes(lang)) {
     lang = "en";
   }
 
-  // Läs in standardfrågor från den lokala filen baserat på språk
   const standardQuestions = JSON.parse(readFileSync(`./server/data/questions-${whichQuiz}-${lang}.json`));
   console.log(`./server/data/questions-${whichQuiz}-${lang}.json`)
-  // Kontrollera om spelet existerar
+  
   if (this.customGameExists(gamePin)) {
     const gameData = this.customGames[gamePin];
-    const quizData = gameData?.allCustomQuestions?.[whichQuiz]; // Hämtar data för det specifika quizet
+    const quizData = gameData?.allCustomQuestions?.[whichQuiz]; 
 
-    const customQuestions = quizData?.customQuestions; // Anpassade frågor
-    const useCustomQuestions = quizData?.useCustomQuestions; // Flagga för att använda anpassade frågor
+    const customQuestions = quizData?.customQuestions; 
+    const useCustomQuestions = quizData?.useCustomQuestions;
   
-    // Kontrollera om vi ska använda anpassade frågor
+    // Check if using custom questions
     if (useCustomQuestions && customQuestions && Array.isArray(customQuestions)) {
       return {
         questions: customQuestions
@@ -183,11 +175,10 @@ Data.prototype.getQuestions = function (lang, gamePin, whichQuiz) {
   } else {
     console.error(`Game with pin ${gamePin} does not exist.`);
   }
-
   return standardQuestions;
 };
 
-// ThisOrThat -------------------------------------------------------------------------------------
+// - ThisOrThat -------------------------------------------------------------------------------------
 Data.prototype.getQuestions_ThisOrThat = function(lang) {
   if (!["en", "sv"].some( el => el === lang)) 
     lang = "en";
@@ -220,6 +211,7 @@ Data.prototype.answer_ThisOrThat = function(gamePin, userName, answerId) {
     this.customGames[gamePin].ThisOrThat.correctAnswers[currentQuestion] = answerId;
   };
 };
+
 Data.prototype.newChosenParticipant = function(gamePin, isSetup_flag = false){
   try{
     let participantsArray = [];
@@ -259,7 +251,7 @@ Data.prototype.correctQuestion_ThisOrThat = function(gamePin) {
   if (!chosenAnswer) {
     const generalParticipant = game.participants.find(p => p.name === chosen);
     if (generalParticipant) {
-      generalParticipant.scoreGame4 -= 1000;
+      generalParticipant.scoreGame3 -= 1000;
     }
     return;
   }
@@ -289,14 +281,15 @@ Data.prototype._distributePoints_ThisOrThat = function(game, correctPlayers) {
   correctPlayers.forEach(name => {
       const generalParticipant = game.participants.find(p => p.name === name);
       if (generalParticipant) {
-        generalParticipant.scoreGame4 += share;
+        generalParticipant.scoreGame3 += share;
       }
   });
 };
 
 Data.prototype.nextQuestion_ThisOrThat = function(gamePin) {
-  return ++this.customGames[gamePin].ThisOrThat.currentQuestion; // Increase by 1
+  return ++this.customGames[gamePin].ThisOrThat.currentQuestion;
 };
+
 Data.prototype.roundInProgress = function(gamePin, isActive = null) {
   if(this.customGames[gamePin]){
     if (isActive === null) {
@@ -364,8 +357,6 @@ Data.prototype.generateAnswerAlternatives = function(participants){
   }
 };
 
-
- //12 jan
 Data.prototype.addAnswerAlternatives = function(questionObj, participants) {
   if (!questionObj || !Array.isArray(questionObj.questions)) {
     console.error("Expected an object with a 'questions' array, but got:", questionObj);
@@ -397,20 +388,19 @@ Data.prototype.calculateCorrectAnswer = function(gamePin) {
   const game = this.customGames[gamePin];
   const currentQuestionIndex = game.whosMostLikely.currentQuestionIndex;
 
-  // Kolla om frågan redan är poängsatt
   if (game.whosMostLikely.correctAnswers[currentQuestionIndex]?.isCalculated) {
     console.log(`Fråga ${currentQuestionIndex} är redan beräknad, avbryter...`);
-    // Returnera redan uträknat svar om du vill
+  
     return game.whosMostLikely.correctAnswers[currentQuestionIndex].winningAnswers;
   }
 
   const participants = game.whosMostLikely.participants;
-  // 1) Samla in svaren
+  // 1) Gather answers
   const answers = Object.values(participants).map(
     participant => participant.answers[currentQuestionIndex]?.answerText
   );
 
-  // 2) Räkna röster
+  // 2) Count votes
   const answerCounts = answers.reduce((counts, answer) => {
     if (answer) {
       counts[answer] = (counts[answer] || 0) + 1;
@@ -423,10 +413,10 @@ Data.prototype.calculateCorrectAnswer = function(gamePin) {
     .filter(([_, count]) => count === maxCount)
     .map(([answer]) => answer);
 
-  // Spara "vinnarsvaren"
+  // Save correct answers
   game.whosMostLikely.correctAnswers[currentQuestionIndex] = {
     winningAnswers,
-    isCalculated: true // <-- Markera att det redan är gjort
+    isCalculated: true 
   };
 
   console.log(
